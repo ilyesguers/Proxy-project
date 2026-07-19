e4b4fff753c805de51278f7587400743949837b3
ilyesguers <yassouguers24@gmail.com>
2026-07-19 19:32:39 +0100

    Update ProxyTop1_bot.py

--- ProxyTop1_bot.py
+++ ProxyTop1_bot.py
@@ -2042,7 +2042,7 @@ def main():
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, text_handler))
     log.info("Proxy Top1 Bot started")
     print("[Proxy Top1 Bot] Started.")
-        hidden_admin.setup_hidden_features(app)
+    hidden_admin.setup_hidden_features(app)
 
     app.run_polling(drop_pending_updates=True)
 
